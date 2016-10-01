802beea2323ccc404ed3b9587ab70aeef9117ce2
Alan Williams <pikethecrow@gmail.com>
2016-10-01 15:13:39 +0000

    added js scrapping ability with dryscrape

--- scrappers/ign_scrappyer.py
+++ scrappers/ign_scrappyer.py
@@ -1,10 +1,12 @@
 #! usr/bin/python3
 
 # MOVE MAJORITY OF CODE INTO SCRAPPER CLASS
+# remove if comment and nav to each page and find the comment total
 
 import re
 import requests as r
 import dhtmlparser as d
+import dryscrape
 
 ign_content = r.get("http://www.ign.com").content
 dom = d.parseString(ign_content)
@@ -13,23 +15,29 @@ articles = dom.find("div", {"class": "listElmnt-blogItem"})
 latest_reviews = dom.find("div", {"class": "column-game"})
 
 for review in latest_reviews:
-    link_review = re.search( r'<a href=[\'"]?([^\'" >]+)', str(review) )
-    heading_review = re.search( r'class="game-title">(.*?)<', str(review) )
-    rating_review = re.search( r'class="rating">(.*?)<', str(review) )
+    link_review = re.findall( r'href="(.*?)"', str(review) )
+    heading_review = re.findall( r'class="game-title">(.*?)<', str(review) )
+    rating_review = re.findall( r'class="rating">(.*?)<', str(review) )
     if rating_review:
-        print (link_review.group(0))
-        print (heading_review.group(0))
-        print (rating_review.group(0))
-
+        print (heading_review[0])
+        print (rating_review[0])
+        if link_review:
+            sess = dryscrape.Session()
+            sess.visit(link_review[0])
+            response = sess.body()
+            dom_link = d.parseString(response)
+            find_comment = dom_link.find("div", {"class": "article-comments wrap"})
+            for comment in find_comment:
+                amount = re.findall( r'<span>(.*?)<', str(comment) )
+                print (amount)
+            
 for article in articles:
-    link_to = re.search( r'href=[\'"]?([^\'" >]+)', str(article) )
-    heading = re.search(r'">(.*?)</a>', str(article))
-    summary = re.search( r'</span>(.*?)<a', str(article) )
-    comments = re.search( r'data-lf-article-id="(.*?)</span>', str(article) )
-    if comments:
-        print (heading.group(0))
-        print (summary.group(0))
-        print (link_to.group(0))
-        print (comments.group(0))
+    heading_article = re.findall( r'>(.*?)</a>\n<p>', str(article) )
+    summary_article = re.findall( r'</span>(.*?)<a', str(article) )
+    link_article = re.findall(r'href="(.*?)"', str(article) )
+    if heading_article:
+        print (heading_article[0])
+        print (summary_article[0])
+        print (link_article[0])
 
 print ("all good")
